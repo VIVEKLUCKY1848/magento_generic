8e47ee9be0cbab486368530ba22b31a00c3ca11b
VS <viveks@elayers.net>
2015-08-06 00:43:34 +0530

    Update magento_core_snippets.php

--- magento_core_snippets.php
+++ magento_core_snippets.php
@@ -1,16 +1,24 @@
 <?php
 
 ## Fetch products with status "Enabled" and visible in both catalog & search.
+$visibility = array(
+  Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH,
+  Mage_Catalog_Model_Product_Visibility::VISIBILITY_IN_CATALOG
+);
 $products = Mage::getModel('catalog/product')->getCollection();
 $products->addAttributeToSelect('*');
 $products->addAttributeToFilter('status', 1);
 $products->addAttributeToFilter('visibility', 4);
+## or ##
+$products->addAttributeToFilter('visibility', $visibility);
 
 ## Get categories of only enabled and visible products
 $categories = array();
 $products = Mage::getModel('catalog/product')->getCollection();
 $products->addAttributeToFilter('status', 1);
 $products->addAttributeToFilter('visibility', 4);
+## or ##
+$products->addAttributeToFilter('visibility', $visibility);
 foreach($products as $product) {
   foreach($product->getCategoryIds() as $cat) {
      $categories[] = $cat;
