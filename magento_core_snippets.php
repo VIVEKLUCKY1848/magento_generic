a748549564a807ac139b37b17f2464d49a5170aa
VS <viveks@elayers.net>
2015-08-06 00:37:29 +0530

    Update magento_core_snippets.php

--- magento_core_snippets.php
+++ magento_core_snippets.php
@@ -6,4 +6,14 @@ $products->addAttributeToSelect('*');
 $products->addAttributeToFilter('status', 1);
 $products->addAttributeToFilter('visibility', 4);
 
+## Get categories of only enabled and visible products
+$categories = array();
+$products = Mage::getModel('catalog/product')->getCollection()->addAttributeToFilter('visibility', 4);
+foreach($products as $product) {
+  foreach($product->getCategoryIds() as $cat) {
+     $categories[] = $cat;
+  }
+}
+$categories = array_values(array_unique($categories));
+
 ?>
